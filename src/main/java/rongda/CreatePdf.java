ed0392fdd3b12b242a7e77e42d62d6f5b21bf8db
WinchongYoung <ywz745827328@gmail.com>
2023-01-08 11:04:41 +0800

    提交

--- src/main/java/rongda/CreatePdf.java
+++ src/main/java/rongda/CreatePdf.java
@@ -8,6 +8,7 @@ import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.PdfWriter;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,14 +23,14 @@ public class CreatePdf {
         PdfAcroForm pdfAcroForm = PdfAcroForm.getAcroForm(pdfDoc, true);
         //原始数据(待填充到PDF文件中的数据)
         Map<String, String> formDatas = new HashMap<>();
-        formDatas.put("fill_1","总理斯蒂芬拉萨大家ask地方啦爱丽丝打开房间阿里山的开发啊拉萨看得见伐啦");
-        formDatas.put("fill_2","2023");
-        formDatas.put("fill_3","    1");
-        formDatas.put("fill_4","   8");
-        formDatas.put("fill_5","5");
-        formDatas.put("fill_6","6");
-        formDatas.put("fill_7","7");
-        formDatas.put("fill_8","8");
+        formDatas.put("fill_1", "总理斯蒂芬拉萨大家ask地方啦爱丽丝打开房间阿里山的开发啊拉萨看得见伐啦");
+        formDatas.put("fill_2", "2023");
+        formDatas.put("fill_3", "    1");
+        formDatas.put("fill_4", "   8");
+        formDatas.put("fill_5", "5");
+        formDatas.put("fill_6", "6");
+        formDatas.put("fill_7", "7");
+        formDatas.put("fill_8", "8");
         //获取模板文件中的全量字段
         Map<String, PdfFormField> formFields = pdfAcroForm.getFormFields();
         formFields.forEach((key, value) -> {
@@ -45,6 +46,29 @@ public class CreatePdf {
                 e.printStackTrace();
             }
 
+        });
+        //设置生成表单不可编辑，注意itext的版本，在7.0.0版本报错
+        pdfAcroForm.flattenFields();
+        pdfDoc.close();
+    }
+
+    public static void pdfGenerator(String filePath, String toPath, Map<String, String> formDatas) throws IOException {
+        PdfDocument pdfDoc = new PdfDocument(new PdfReader(filePath), new PdfWriter(toPath));
+        PdfAcroForm pdfAcroForm = PdfAcroForm.getAcroForm(pdfDoc, true);
+        //获取模板文件中的全量字段
+        Map<String, PdfFormField> formFields = pdfAcroForm.getFormFields();
+        formFields.forEach((key, value) -> {
+            try {
+                //处理中文乱码
+                PdfFont font = PdfFontFactory.createFont("STSongStd-Light", "UniGB-UCS2-H", false);
+                //获取到模板中的某个字段
+                PdfFormField agreementId = formFields.get(key);
+                agreementId.setFont(font);
+                //为模板中的某个字段设置值
+                agreementId.setValue(formDatas.get(key));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         });
         //设置生成表单不可编辑，注意itext的版本，在7.0.0版本报错
         pdfAcroForm.flattenFields();
--- src/main/java/rongda/ReadExcel.java
+++ src/main/java/rongda/ReadExcel.java
@@ -1,6 +1,5 @@
 package rongda;
 
-import org.apache.commons.collections.ArrayStack;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.ss.usermodel.Row;
@@ -8,16 +7,17 @@ import org.apache.poi.ss.util.NumberToTextConverter;
 import org.apache.poi.xssf.usermodel.XSSFFormulaEvaluator;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.redisson.mapreduce.Collector;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class ReadExcel {
+    public static DecimalFormat df = new DecimalFormat("#");
 
     public static void main(String[] args) throws IOException {
 
@@ -50,15 +50,12 @@ public class ReadExcel {
                 Cell cell = cellIterator.next();
                 switch (cell.getCellType()) {
                     case Cell.CELL_TYPE_STRING:
-                        // System.out.print(cell.getStringCellValue() + "\t");
                         tmp.add(cell.getStringCellValue());
                         break;
                     case Cell.CELL_TYPE_NUMERIC:
-                        // System.out.print(cell.getNumericCellValue() + "\t");
                         tmp.add(cell.getNumericCellValue());
                         break;
                     case Cell.CELL_TYPE_BOOLEAN:
-                        // System.out.print(cell.getBooleanCellValue() + "\t");
                         tmp.add(cell.getBooleanCellValue());
                         break;
                     case Cell.CELL_TYPE_FORMULA:
@@ -93,9 +90,42 @@ public class ReadExcel {
             result.add(lists);
         }
 
+        // 获取银行卡号表
+        Map<String, List<String>> cardMap = new HashMap<>();
+        FileInputStream mangerInfoFile = new FileInputStream(new File("C:\\Users\\74582\\Desktop\\管理人员银行卡号.xlsx"));
+        XSSFWorkbook managerINfoXss = new XSSFWorkbook(mangerInfoFile);
+        XSSFSheet managerSheet = managerINfoXss.getSheetAt(0);
+        Iterator<Row> rowIterator1 = managerSheet.iterator();
+        while (rowIterator1.hasNext()) {
+            Row row2 = rowIterator1.next();
+            String nameTmp = "";
+            if (row2.getCell(1) != null) {
+                row2.getCell(1).setCellType(Cell.CELL_TYPE_STRING);
+                nameTmp = row2.getCell(1).getStringCellValue();
+            }
+
+            String cardNumTmp = "";
+            if (row2.getCell(6) != null) {
+                cardNumTmp = getCellValue(row2.getCell(6)).toString();
+            }
+
+            String bankName = "";
+            if (row2.getCell(7) != null) {
+                row2.getCell(7).setCellType(Cell.CELL_TYPE_STRING);
+                bankName = row2.getCell(7).getStringCellValue();
+            }
+            List<String> single = new ArrayList<>();
+            single.add(cardNumTmp);
+            single.add(bankName);
+            cardMap.put(nameTmp, single);
+        }
+        // cardMap.forEach((x, y) -> System.out.println(x + "->" + y));
+        // System.out.println(cardMap.size());
+        // System.exit(0);
+
         for (List<List<Object>> tmp : result) {
             // 姓名处理
-            String name = tmp.get(1).get(0).toString().replace("姓名", "").replace("：", "");
+            String name = tmp.get(1).get(0).toString().replace("姓名", "").replace("：", "").replace(" ", "");
             // 工地处理
             Set<String> set = new HashSet<>();
             for (int z = 3; z < 8; z++) {
@@ -106,15 +136,58 @@ public class ReadExcel {
             // 大写金额处理
             String totalStr = tmp.get(20).get(4).toString();
             String totalCN = totalStr.replace("（", "").replace("）", "").replace("大写", "").replace("：", "").replace(" ", "");
+
+            // 银行卡号
+            String cardNum = cardMap.containsKey(name) ? cardMap.get(name).get(0) : "";
+            // 开户行
+            String bankName = cardMap.containsKey(name) ? cardMap.get(name).get(1) : "";
+
             people.add(new PersonBean(name, totalCN,
-                    new BigDecimal(tmp.get(20).get(2).toString()).setScale(0, BigDecimal.ROUND_HALF_UP).toString(), set, "", ""));
+                    new BigDecimal(tmp.get(20).get(2).toString()).setScale(0, BigDecimal.ROUND_HALF_UP).toString() + ".00",
+                    set,
+                    cardNum, bankName));
         }
 
         List<PersonBean> collect = people.stream().filter(x -> !x.name.trim().equals("")).collect(Collectors.toList());
 
         for (PersonBean per : collect) {
+            Map<String, String> params = new HashMap<>();
+            params.put("fill_1", per.getWorkSpaces().stream().collect(Collectors.joining(" ")));
+            params.put("fill_2", "2023");
+            params.put("fill_3", "1");
+            params.put("fill_4", "8");
+            params.put("fill_5", "支付" + per.getName() + "2022年余下全部工资");
+            params.put("fill_6", per.getTotalCN());
+            params.put("fill_7", per.getTotal());
+            params.put("fill_8", per.getCardNum() + "  " + per.getBankDesc());
+            CreatePdf.pdfGenerator("C:\\Users\\74582\\Desktop\\haha.pdf", "C:\\Users\\74582\\Desktop\\pdf_output\\" + per.name + ".pdf", params);
             System.out.println(per);
         }
 
+        // 生成pdf
+
+
     }
+
+    public static Object getCellValue(Cell cell) {
+        Object value = "";
+        switch (cell.getCellType()) {
+            case Cell.CELL_TYPE_STRING:
+                value = cell.getStringCellValue();
+                break;
+            case Cell.CELL_TYPE_NUMERIC:
+                value = String.valueOf(df.format(cell.getNumericCellValue()));;
+                break;
+            case Cell.CELL_TYPE_BOOLEAN:
+                value = cell.getBooleanCellValue();
+                break;
+            case Cell.CELL_TYPE_FORMULA:
+                value = NumberToTextConverter.toText(cell.getNumericCellValue());
+                break;
+            default:
+
+        }
+        return value;
+    }
+
 }
