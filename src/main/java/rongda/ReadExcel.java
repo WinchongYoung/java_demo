b6e95d77914a98bde137a922165aa9d29c12c6ba
WinchongYoung <ywz745827328@gmail.com>
2023-01-08 22:44:56 +0800

    fix

--- src/main/java/rongda/ReadExcel.java
+++ src/main/java/rongda/ReadExcel.java
@@ -21,150 +21,177 @@ public class ReadExcel {
 
     public static void main(String[] args) throws IOException {
 
+        File dir = new File("C:\\Users\\74582\\Desktop\\input_excel");
 
-        List<PersonBean> people = new ArrayList<>();
+        File[] files = dir.listFiles();
 
-        List<List<Object>> list = new ArrayList<>();
+        for (File tmpFile : files) {
+            FileInputStream fis = new FileInputStream(tmpFile.getAbsolutePath());
 
-        FileInputStream fis = new FileInputStream(new File("C:\\Users\\74582\\Desktop\\gaga.xlsx"));
+            System.out.println(tmpFile.getAbsolutePath());
+            // Finds the workbook instance for XLSX file
+            XSSFWorkbook myWorkBook = new XSSFWorkbook(fis);
 
-        // Finds the workbook instance for XLSX file
-        XSSFWorkbook myWorkBook = new XSSFWorkbook(fis);
+            int numberOfSheets = myWorkBook.getNumberOfSheets();
+            for (int sheetIndex = 0; sheetIndex < numberOfSheets; sheetIndex++) {
 
-        // Return first sheet from the XLSX workbook
-        XSSFSheet mySheet = myWorkBook.getSheetAt(1);
+                List<PersonBean> people = new ArrayList<>();
 
-        // Get iterator to all the rows in current sheet
-        Iterator<Row> rowIterator = mySheet.iterator();
+                List<List<Object>> list = new ArrayList<>();
 
-        // Traversing over each row of XLSX file
-        FormulaEvaluator formulaEvaluator = new XSSFFormulaEvaluator(myWorkBook);
+                XSSFSheet mySheet = myWorkBook.getSheetAt(sheetIndex);
+                System.out.println(mySheet.getSheetName());
+                // Get iterator to all the rows in current sheet
+                Iterator<Row> rowIterator = mySheet.iterator();
 
-        while (rowIterator.hasNext()) {
-            Row row = rowIterator.next();
-            // For each row, iterate through each columns
-            Iterator<Cell> cellIterator = row.cellIterator();
-            List<Object> tmp = new ArrayList<>();
-            while (cellIterator.hasNext()) {
+                // Traversing over each row of XLSX file
+                FormulaEvaluator formulaEvaluator = new XSSFFormulaEvaluator(myWorkBook);
 
-                Cell cell = cellIterator.next();
-                switch (cell.getCellType()) {
-                    case Cell.CELL_TYPE_STRING:
-                        tmp.add(cell.getStringCellValue());
-                        break;
-                    case Cell.CELL_TYPE_NUMERIC:
-                        tmp.add(cell.getNumericCellValue());
-                        break;
-                    case Cell.CELL_TYPE_BOOLEAN:
-                        tmp.add(cell.getBooleanCellValue());
-                        break;
-                    case Cell.CELL_TYPE_FORMULA:
-                        tmp.add(NumberToTextConverter.toText(cell.getNumericCellValue()));
-                        break;
-                    default:
-                        tmp.add("");
+                while (rowIterator.hasNext()) {
+                    Row row = rowIterator.next();
+                    // For each row, iterate through each columns
+                    Iterator<Cell> cellIterator = row.cellIterator();
+                    List<Object> tmp = new ArrayList<>();
+                    while (cellIterator.hasNext()) {
+
+                        Cell cell = cellIterator.next();
+                        switch (cell.getCellType()) {
+                            case Cell.CELL_TYPE_STRING:
+                                tmp.add(cell.getStringCellValue());
+                                break;
+                            case Cell.CELL_TYPE_NUMERIC:
+                                tmp.add(cell.getNumericCellValue());
+                                break;
+                            case Cell.CELL_TYPE_BOOLEAN:
+                                tmp.add(cell.getBooleanCellValue());
+                                break;
+                            case Cell.CELL_TYPE_FORMULA:
+                                tmp.add(NumberToTextConverter.toText(cell.getNumericCellValue()));
+                                break;
+                            default:
+                                tmp.add("");
+                        }
+                    }
+                    list.add(tmp);
                 }
-            }
-            list.add(tmp);
-        }
 
-        System.out.println(list.get(0).get(0));
+                // System.out.println(list.get(0).get(0));
 
-        // 分组
-        Map<Integer, Integer> map = new HashMap<>();
-        List<List<Integer>> indexRecord = new ArrayList<>();
-        Integer startIndex = 0;
+                // 分组
+                List<List<Integer>> indexRecord = new ArrayList<>();
+                Integer startIndex = 0;
 
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).size() > 0 && list.get(i).get(0).toString().contains("北京荣达永发建设工程有限公司2022")) {
-                startIndex = i;
-            }
-            if (list.get(i).size() > 0 && list.get(i).get(0).toString().contains("电话")) {
-                indexRecord.add(Arrays.asList(startIndex, i));
-            }
-        }
-
-        List<List<List<Object>>> result = new ArrayList<>();
-        for (List<Integer> tmp : indexRecord) {
-            List<List<Object>> lists = list.subList(tmp.get(0), tmp.get(1) + 1);
-            result.add(lists);
-        }
-
-        // 获取银行卡号表
-        Map<String, List<String>> cardMap = new HashMap<>();
-        FileInputStream mangerInfoFile = new FileInputStream(new File("C:\\Users\\74582\\Desktop\\管理人员银行卡号.xlsx"));
-        XSSFWorkbook managerINfoXss = new XSSFWorkbook(mangerInfoFile);
-        XSSFSheet managerSheet = managerINfoXss.getSheetAt(0);
-        Iterator<Row> rowIterator1 = managerSheet.iterator();
-        while (rowIterator1.hasNext()) {
-            Row row2 = rowIterator1.next();
-            String nameTmp = "";
-            if (row2.getCell(1) != null) {
-                row2.getCell(1).setCellType(Cell.CELL_TYPE_STRING);
-                nameTmp = row2.getCell(1).getStringCellValue();
-            }
-
-            String cardNumTmp = "";
-            if (row2.getCell(6) != null) {
-                cardNumTmp = getCellValue(row2.getCell(6)).toString();
-            }
-
-            String bankName = "";
-            if (row2.getCell(7) != null) {
-                row2.getCell(7).setCellType(Cell.CELL_TYPE_STRING);
-                bankName = row2.getCell(7).getStringCellValue();
-            }
-            List<String> single = new ArrayList<>();
-            single.add(cardNumTmp);
-            single.add(bankName);
-            cardMap.put(nameTmp, single);
-        }
-        // cardMap.forEach((x, y) -> System.out.println(x + "->" + y));
-        // System.out.println(cardMap.size());
-        // System.exit(0);
-
-        for (List<List<Object>> tmp : result) {
-            // 姓名处理
-            String name = tmp.get(1).get(0).toString().replace("姓名", "").replace("：", "").replace(" ", "");
-            // 工地处理
-            Set<String> set = new HashSet<>();
-            for (int z = 3; z < 8; z++) {
-                if (!tmp.get(z).get(0).toString().trim().equals("")) {
-                    set.add(tmp.get(z).get(0).toString().trim());
+                for (int i = 0; i < list.size(); i++) {
+                    if (list.get(i).size() > 0 && list.get(i).get(0).toString().contains("北京荣达永发建设工程有限公司2022")) {
+                        startIndex = i;
+                    }
+                    if (list.get(i).size() > 0 && list.get(i).get(0).toString().contains("本人确认")) {
+                        indexRecord.add(Arrays.asList(startIndex, i));
+                    }
                 }
+
+                List<List<List<Object>>> result = new ArrayList<>();
+                for (List<Integer> tmp : indexRecord) {
+                    List<List<Object>> lists = list.subList(tmp.get(0), tmp.get(1) + 1);
+                    result.add(lists);
+                }
+
+                /*indexRecord.forEach(System.out::println);
+                System.exit(0);*/
+                // 获取银行卡号表
+                Map<String, List<String>> cardMap = new HashMap<>();
+                FileInputStream mangerInfoFile = new FileInputStream(new File("C:\\Users\\74582\\Desktop\\person_info\\person_info.xlsx"));
+                XSSFWorkbook managerINfoXss = new XSSFWorkbook(mangerInfoFile);
+                XSSFSheet managerSheet = managerINfoXss.getSheetAt(0);
+                Iterator<Row> rowIterator1 = managerSheet.iterator();
+                while (rowIterator1.hasNext()) {
+                    Row row2 = rowIterator1.next();
+                    String nameTmp = "";
+                    if (row2.getCell(0) != null) {
+                        row2.getCell(0).setCellType(Cell.CELL_TYPE_STRING);
+                        nameTmp = row2.getCell(0).getStringCellValue().replace(" ", "");
+                    }
+
+                    String cardNumTmp = "";
+                    if (row2.getCell(1) != null) {
+                        cardNumTmp = getCellValue(row2.getCell(1)).toString();
+                    }
+
+                    String bankName = "";
+                    if (row2.getCell(2) != null) {
+                        row2.getCell(2).setCellType(Cell.CELL_TYPE_STRING);
+                        bankName = row2.getCell(2).getStringCellValue();
+                    }
+                    List<String> single = new ArrayList<>();
+                    single.add(cardNumTmp);
+                    single.add(bankName);
+                    cardMap.put(nameTmp, single);
+                }
+
+                // cardMap.forEach((x, y) -> System.out.println(x + "->" + y));
+
+                for (List<List<Object>> tmp : result) {
+                    // 姓名处理
+                    String name = tmp.get(1).get(0).toString().replace("姓名", "").replace("：", "").replace(" ", "");
+                    /*if (name.equals("常红星")) {
+                        System.exit(0);
+                    }*/
+                    // 工地处理
+                    Set<String> set = new HashSet<>();
+                    for (int z = 3; z < 8; z++) {
+                        if (!tmp.get(z).get(0).toString().trim().equals("")) {
+                            set.add(tmp.get(z).get(0).toString().trim());
+                        }
+                    }
+
+                    // 数字金额
+                    String moneyOri = tmp.get(20).get(2).toString().equals("") ? "0" : tmp.get(20).get(2).toString();
+                    String money = new BigDecimal(moneyOri).setScale(0, BigDecimal.ROUND_HALF_UP).toString();
+
+                    // 大写金额处理
+                    String totalStr = tmp.get(20).get(4).toString();
+                    String totalCN = totalStr.replace("（", "").replace("）", "").replace("大写", "").replace("：", "").replace(" ", "");
+                    if (!money.equals("") && (totalCN.equals("元整") || totalCN.equals(""))) {
+                        totalCN = ConvertUpMoney.toChinese(money) + "元整";
+                    }
+
+                    // 银行卡号
+                    String cardNum = cardMap.containsKey(name) ? cardMap.get(name).get(0) : "";
+                    // 开户行
+                    String bankName = cardMap.containsKey(name) ? cardMap.get(name).get(1) : "";
+
+                    people.add(new PersonBean(name, totalCN,
+                            money + ".00",
+                            set,
+                            cardNum, bankName));
+                }
+                // people.forEach(System.out::println);
+                // System.exit(0);
+                List<PersonBean> collect = people.stream().filter(x -> !x.name.trim().equals("")).collect(Collectors.toList());
+
+                for (PersonBean per : collect) {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("fill_1", per.getWorkSpaces().stream().collect(Collectors.joining("、")));
+                    params.put("fill_2", "2023");
+                    params.put("fill_3", "1");
+                    params.put("fill_4", "8");
+                    params.put("fill_5", "支付" + per.getName() + "2022年余下全部工资");
+                    params.put("fill_6", per.getTotalCN());
+                    params.put("fill_7", per.getTotal());
+                    params.put("fill_8", per.getCardNum() + "  " + per.getBankDesc());
+                    Random random = new Random();
+                    int randomNum = random.nextInt(10000);
+                    CreatePdf.pdfGenerator("C:\\Users\\74582\\Desktop\\haha.pdf", "C:\\Users\\74582\\Desktop\\pdf_output\\" + per.name + ".pdf", params);
+                    // System.out.println(per);
+                }
+
+                System.out.println("生成pdf成功，总计" + collect.size());
+
+                // 生成pdf
             }
-            // 大写金额处理
-            String totalStr = tmp.get(20).get(4).toString();
-            String totalCN = totalStr.replace("（", "").replace("）", "").replace("大写", "").replace("：", "").replace(" ", "");
 
-            // 银行卡号
-            String cardNum = cardMap.containsKey(name) ? cardMap.get(name).get(0) : "";
-            // 开户行
-            String bankName = cardMap.containsKey(name) ? cardMap.get(name).get(1) : "";
 
-            people.add(new PersonBean(name, totalCN,
-                    new BigDecimal(tmp.get(20).get(2).toString()).setScale(0, BigDecimal.ROUND_HALF_UP).toString() + ".00",
-                    set,
-                    cardNum, bankName));
         }
-
-        List<PersonBean> collect = people.stream().filter(x -> !x.name.trim().equals("")).collect(Collectors.toList());
-
-        for (PersonBean per : collect) {
-            Map<String, String> params = new HashMap<>();
-            params.put("fill_1", per.getWorkSpaces().stream().collect(Collectors.joining(" ")));
-            params.put("fill_2", "2023");
-            params.put("fill_3", "1");
-            params.put("fill_4", "8");
-            params.put("fill_5", "支付" + per.getName() + "2022年余下全部工资");
-            params.put("fill_6", per.getTotalCN());
-            params.put("fill_7", per.getTotal());
-            params.put("fill_8", per.getCardNum() + "  " + per.getBankDesc());
-            CreatePdf.pdfGenerator("C:\\Users\\74582\\Desktop\\haha.pdf", "C:\\Users\\74582\\Desktop\\pdf_output\\" + per.name + ".pdf", params);
-            System.out.println(per);
-        }
-
-        // 生成pdf
 
 
     }
@@ -176,7 +203,8 @@ public class ReadExcel {
                 value = cell.getStringCellValue();
                 break;
             case Cell.CELL_TYPE_NUMERIC:
-                value = String.valueOf(df.format(cell.getNumericCellValue()));;
+                value = String.valueOf(df.format(cell.getNumericCellValue()));
+                ;
                 break;
             case Cell.CELL_TYPE_BOOLEAN:
                 value = cell.getBooleanCellValue();
