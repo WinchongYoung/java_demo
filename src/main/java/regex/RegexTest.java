4ccefdc534aa51c4983def0e57635767f8a634f1
WinchongYoung <ywz745827328@gmail.com>
2023-01-10 09:11:33 +0800

    regex

---
+++ src/main/java/regex/RegexTest.java
@@ -0,0 +1,21 @@
+package regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 正则
+ */
+public class RegexTest {
+    public static void main(String[] args) {
+
+        String jsonString = "{\"id\":\"123123\",\"name\":\"gaga\"}";
+        //
+        String regex = "\"name\":\"(.*?)\\\"";
+        Matcher matcher = Pattern.compile(regex).matcher(jsonString);
+        while (matcher.find()) {
+            System.out.println(matcher.group(1));
+        }
+    }
+
+}
