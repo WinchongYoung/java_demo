4aaf132824b0568f645fb3fafbcedf43f7973d8e
WinchongYoung <ywz745827328@gmail.com>
2023-01-11 10:06:01 +0800

    fix merge pdfs

--- src/main/java/rongda/ReadExcel3.java
+++ src/main/java/rongda/ReadExcel3.java
@@ -12,18 +12,22 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.text.DecimalFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class ReadExcel {
-    public static DecimalFormat df = new DecimalFormat("#");
+public class ReadExcel3 {
 
     public static void main(String[] args) throws IOException {
 
-        File dir = new File("C:\\Users\\74582\\Desktop\\input_excel");
+        String homeDir = args[0];
+        String year = args[1], mon = args[2], day = args[3];
+
+        File dir = new File(homeDir + "\\input_excel");
 
         File[] files = dir.listFiles();
+
+        // 获取银行卡号表
+        Map<String, List<String>> cardMap = Utils.getBankInfo(homeDir);
 
         for (File tmpFile : files) {
             FileInputStream fis = new FileInputStream(tmpFile.getAbsolutePath());
@@ -33,6 +37,9 @@ public class ReadExcel {
             XSSFWorkbook myWorkBook = new XSSFWorkbook(fis);
 
             int numberOfSheets = myWorkBook.getNumberOfSheets();
+
+            // 生成前创建目录
+            String savePath = homeDir + "\\out_put\\" + tmpFile.getName().replace(".xlsx", "");
             for (int sheetIndex = 0; sheetIndex < numberOfSheets; sheetIndex++) {
 
                 List<PersonBean> people = new ArrayList<>();
@@ -75,14 +82,11 @@ public class ReadExcel {
                     list.add(tmp);
                 }
 
-                // System.out.println(list.get(0).get(0));
-
                 // 分组
                 List<List<Integer>> indexRecord = new ArrayList<>();
                 Integer startIndex = 0;
-
                 for (int i = 0; i < list.size(); i++) {
-                    if (list.get(i).size() > 0 && list.get(i).get(0).toString().contains("北京荣达永发建设工程有限公司2022")) {
+                    if (list.get(i).size() > 0 && list.get(i).get(0).toString().contains("北京荣达永发建设工程有限公司20")) {
                         startIndex = i;
                     }
                     if (list.get(i).size() > 0 && list.get(i).get(0).toString().contains("本人确认")) {
@@ -96,46 +100,9 @@ public class ReadExcel {
                     result.add(lists);
                 }
 
-                /*indexRecord.forEach(System.out::println);
-                System.exit(0);*/
-                // 获取银行卡号表
-                Map<String, List<String>> cardMap = new HashMap<>();
-                FileInputStream mangerInfoFile = new FileInputStream(new File("C:\\Users\\74582\\Desktop\\person_info\\person_info.xlsx"));
-                XSSFWorkbook managerINfoXss = new XSSFWorkbook(mangerInfoFile);
-                XSSFSheet managerSheet = managerINfoXss.getSheetAt(0);
-                Iterator<Row> rowIterator1 = managerSheet.iterator();
-                while (rowIterator1.hasNext()) {
-                    Row row2 = rowIterator1.next();
-                    String nameTmp = "";
-                    if (row2.getCell(0) != null) {
-                        row2.getCell(0).setCellType(Cell.CELL_TYPE_STRING);
-                        nameTmp = row2.getCell(0).getStringCellValue().replace(" ", "");
-                    }
-
-                    String cardNumTmp = "";
-                    if (row2.getCell(1) != null) {
-                        cardNumTmp = getCellValue(row2.getCell(1)).toString();
-                    }
-
-                    String bankName = "";
-                    if (row2.getCell(2) != null) {
-                        row2.getCell(2).setCellType(Cell.CELL_TYPE_STRING);
-                        bankName = row2.getCell(2).getStringCellValue();
-                    }
-                    List<String> single = new ArrayList<>();
-                    single.add(cardNumTmp);
-                    single.add(bankName);
-                    cardMap.put(nameTmp, single);
-                }
-
-                // cardMap.forEach((x, y) -> System.out.println(x + "->" + y));
-
                 for (List<List<Object>> tmp : result) {
                     // 姓名处理
                     String name = tmp.get(1).get(0).toString().replace("姓名", "").replace("：", "").replace(" ", "");
-                    /*if (name.equals("常红星")) {
-                        System.exit(0);
-                    }*/
                     // 工地处理
                     Set<String> set = new HashSet<>();
                     for (int z = 3; z < 8; z++) {
@@ -165,57 +132,43 @@ public class ReadExcel {
                             set,
                             cardNum, bankName));
                 }
-                // people.forEach(System.out::println);
-                // System.exit(0);
+                // 过滤没有姓名的数据
                 List<PersonBean> collect = people.stream().filter(x -> !x.name.trim().equals("")).collect(Collectors.toList());
 
+                // 生成pdf
+                Utils.createDir(savePath);
                 for (PersonBean per : collect) {
                     Map<String, String> params = new HashMap<>();
                     params.put("fill_1", per.getWorkSpaces().stream().collect(Collectors.joining("、")));
-                    params.put("fill_2", "2023");
-                    params.put("fill_3", "1");
-                    params.put("fill_4", "8");
+                    params.put("fill_2", year);
+                    params.put("fill_3", mon);
+                    params.put("fill_4", day);
                     params.put("fill_5", "支付" + per.getName() + "2022年余下全部工资");
                     params.put("fill_6", per.getTotalCN());
                     params.put("fill_7", per.getTotal());
                     params.put("fill_8", per.getCardNum() + "  " + per.getBankDesc());
                     Random random = new Random();
-                    int randomNum = random.nextInt(10000);
-                    CreatePdf.pdfGenerator("C:\\Users\\74582\\Desktop\\haha.pdf", "C:\\Users\\74582\\Desktop\\pdf_output\\" + per.name + ".pdf", params);
-                    // System.out.println(per);
+                    CreatePdf.pdfGenerator(homeDir + "\\source_pdf\\source_pdf.pdf", savePath + "\\" + per.name + ".pdf", params);
                 }
-
                 System.out.println("生成pdf成功，总计" + collect.size());
-
-                // 生成pdf
             }
 
-
+            // 合并pdf
+            File excelDir = new File(savePath);
+            List<String> pdfs = new ArrayList<>();
+            File[] sheetDirs = excelDir.listFiles();
+            for (File tmpDir : sheetDirs) {
+                if (tmpDir.isDirectory()) {
+                    File[] pdfFile = tmpDir.listFiles();
+                    for (File pdf : pdfFile) {
+                        if (pdf.getName().endsWith("pdf")) {
+                            pdfs.add(pdf.getAbsolutePath());
+                        }
+                    }
+                }
+            }
+            PdfUtils.mergePdfFile(pdfs, homeDir + "\\" + tmpFile + ".pdf");
         }
-
-
-    }
-
-    public static Object getCellValue(Cell cell) {
-        Object value = "";
-        switch (cell.getCellType()) {
-            case Cell.CELL_TYPE_STRING:
-                value = cell.getStringCellValue();
-                break;
-            case Cell.CELL_TYPE_NUMERIC:
-                value = String.valueOf(df.format(cell.getNumericCellValue()));
-                ;
-                break;
-            case Cell.CELL_TYPE_BOOLEAN:
-                value = cell.getBooleanCellValue();
-                break;
-            case Cell.CELL_TYPE_FORMULA:
-                value = NumberToTextConverter.toText(cell.getNumericCellValue());
-                break;
-            default:
-
-        }
-        return value;
     }
 
 }
--- src/main/resources/execute.bat
+++ src/main/resources/execute.bat
@@ -1,2 +1,2 @@
-java -classpath demo-1.0-SNAPSHOT-jar-with-dependencies.jar rongda.ReadExcel2 %cd%
+java -classpath demo-1.0-SNAPSHOT-jar-with-dependencies.jar rongda.ReadExcel2 %cd% 2023 1 11
 pause
