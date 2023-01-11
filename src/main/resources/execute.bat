cd95726c9fda7c91cc41c234db9f28f1e5b5c9dd
WinchongYoung <ywz745827328@gmail.com>
2023-01-11 10:22:12 +0800

    优化bat

--- src/main/resources/execute.bat
+++ src/main/resources/execute.bat
@@ -1,2 +1,5 @@
-java -classpath demo-1.0-SNAPSHOT-jar-with-dependencies.jar rongda.ReadExcel2 %cd% 2023 1 11
+set /p year=请输入年:
+set /p mon=请输入月:
+set /p day=请输入日:
+java -classpath demo-1.0-SNAPSHOT-jar-with-dependencies.jar rongda.ReadExcel3 %cd% %year% %mon% %day%
 pause
