8c047904264f823ac2549738ff4f7325cd52d03f
WinchongYoung <ywz745827328@gmail.com>
2023-01-16 17:11:26 +0800

    update bat

--- src/main/resources/execute.bat
+++ src/main/resources/execute.bat
@@ -1,5 +1,5 @@
 set /p year=请输入年:
 set /p mon=请输入月:
 set /p day=请输入日:
-java -classpath demo-1.0-SNAPSHOT-jar-with-dependencies.jar rongda.ReadExcel3 %cd% %year% %mon% %day%
+java -classpath demo-1.0-SNAPSHOT-jar-with-dependencies.jar rongda.ReadExcel4 %cd% %year% %mon% %day%
 pause
