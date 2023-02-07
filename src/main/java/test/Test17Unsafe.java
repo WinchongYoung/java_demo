0b65652a1b9914ea0616e0413577e96774881f3d
WinchongYoung <ywz745827328@gmail.com>
2023-02-07 14:48:43 +0800

    test ohc

---
+++ src/main/java/test/Test17Unsafe.java
@@ -0,0 +1,22 @@
+package test;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+/**
+ * 通过反射获取Unsafe类
+ * 详见:https://tech.meituan.com/2019/02/14/talk-about-java-magic-class-unsafe.html
+ */
+public class Test17Unsafe {
+    public static void main(String[] args) {
+        try {
+            Field field = Unsafe.class.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            Unsafe unsafe = (Unsafe) field.get(null);
+            System.out.println(unsafe);
+        } catch (NoSuchFieldException | IllegalAccessException noSuchFieldException) {
+            noSuchFieldException.printStackTrace();
+        }
+    }
+}
