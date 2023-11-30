5c37b283c15ee03fe5cfd10984fc0c1041a644fd
WinchongYoung <ywz745827328@gmail.com>
2023-11-30 20:18:42 +0800

    arr2

--- src/main/java/algorithm/leecode/arr/Solution206.java
+++ src/main/java/algorithm/leecode/arr/Solution206.java
@@ -1,24 +1,8 @@
-package algorithm.leecode;
+package algorithm.leecode.arr;
+
+import algorithm.util.ListNode;
 
 public class Solution206 {
-
-    public static class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-
-    }
 
     public static ListNode reverseList(ListNode head) {
         // 1. 递归终止条件
@@ -49,10 +33,21 @@ public class Solution206 {
         l1.next = l2;
         l2.next = l3;
 
-        ListNode result = reverseList2(l1);
+        ListNode result = reverseList3(l1);
         while (result != null) {
             System.out.println(result.val);
             result = result.next;
         }
+    }
+
+    public static ListNode reverseList3(ListNode head) {
+        ListNode pre = null, curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
     }
 }
